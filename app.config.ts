948e8be4992928c6a548b66928f599b0e8722e31
AtlasCLT <atlas@cryptolink.tech>
2024-02-04 10:30:16 +0100

    init push

---
+++ app.config.ts
@@ -0,0 +1,33 @@
+export default defineAppConfig({
+  docus: {
+    title: 'CryptoLink.Tech Developers',
+    description: 'CryptoLink.Tech Cross Chain Infrastructure Documentation',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      twitter: 'CryptoLink_Tech',
+      github: 'CryptoLinkTech',
+    },
+    github: {
+      dir: '.starters/default/content',
+      branch: 'main',
+      repo: 'docs-developer',
+      owner: 'CryptoLinkTech',
+      edit: true
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true
+    }
+  }
+})
